922ce3ebf8149fe27e324b407ad5669dc3e63d47
hmdalvin <63193506+hmdalvin@users.noreply.github.com>
2023-04-06 20:23:46 +0700

    add modules

--- belajar-nodejs/contact-app/contacts.js
+++ belajar-nodejs/contact-app/contacts.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
-const { resolve } = require('path');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const chalk = require('chalk');
+const validator = require('validator');
 
 // membuat folder jika belum ada
 const dirPath = './data';
@@ -19,24 +14,37 @@ if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        });
-    });
-};
-
 const simpanContact = (nama, email, noHP) => {
     const contact = { nama, email, noHP };
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(fileBuffer);
 
+    // cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if(duplikat) {
+        console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!'));
+        return false;
+    }
+
+    // cek email
+    if(email) {
+        if(!validator.isEmail(email)) {
+            console.log(chalk.red.inverse.bold('Email tidak valid!'));
+            return false;
+        }
+    }
+
+    // cek no HP
+    if(!validator.isMobilePhone(noHP, 'id-ID')) {
+        console.log(chalk.red.inverse.bold('Nomor tidak tidak valid!'));
+        return false;
+    }
+
     contacts.push(contact);
+
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-    console.log('terimahkasih sudah memasukan data.');
-    rl.close();
+    console.log(chalk.green.inverse.bold('terimahkasih sudah memasukan data.'));
 }
 
-module.exports = { tulisPertanyaan, simpanContact };
+module.exports = { simpanContact };
