83acd2077ea4d2ce80d3893aebaf09ebcdbc3ce9
hmdalvin <63193506+hmdalvin@users.noreply.github.com>
2023-04-07 15:56:10 +0700

    express

---
+++ belajar-nodejs/14-express-web-server/app.js
@@ -0,0 +1,81 @@
+// tutorial 22:36
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello World!</h1>');
+    // res.json({
+    //     nama: "Alfian",
+    //     email: "aafian@gmail.com",
+    //     noHP: '0812234'
+    // });
+    res.sendFile('./index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    // res.send('Ini adalah Halaman About');
+    res.sendFile('./about.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    // res.send('Ini adalah Halaman Contact');
+    res.sendFile('./contact.html', { root: __dirname });
+});
+
+app.get('/product/:id', (req, res) =>{
+    res.send(`Product ID : ${req.params.id} <br> Category : ${req.query.category}`);
+});
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h1>404</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+// const http = require("http");
+// const fs = require('fs');
+
+// const port = 3000;
+
+// const renderHTML = (path, res) => {
+//     fs.readFile(path, (err, data) => {
+//         if(err) {
+//             res.writeHead(404);
+//             res.write('Error: file not found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// };
+
+// http
+//     .createServer((req, res) => {
+//         res.writeHead(200, {
+//             'Content-Type': 'text/html'
+//         });
+
+//         const url = req.url;
+
+//         switch(url) {
+//             case '/about':
+//                 renderHTML('./about.html', res);
+//                 break;
+//             case '/contact':
+//                 renderHTML('./contact.html', res);
+//                 break;
+//             default:
+//                 renderHTML('./index.html', res);
+//                 break;
+//         };
+        
+//     })
+//     .listen(port, () => {
+//         console.log(`Server is listening on port ${port}..`);
+//     });
